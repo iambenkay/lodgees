0bfba5b2e570a05ce7fa325ba012d0c04b2d9d8a
benjamin <developerkayx@gmail.com>
2018-10-14 20:11:56 +0100

    Added supported host

--- unnlodges/settings.py
+++ unnlodges/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'd-he9nun)%skwa3isyk9q_%v33atk)w&f04&*jq72tvvu8^uq6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.43.161', '127.0.0.1', 'localhost', '192.168.43.170']
+ALLOWED_HOSTS = ["199.195.116.100"]
 
 
 # Application definition
